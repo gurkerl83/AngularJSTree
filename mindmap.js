c62498c169b5808a17738c9d03d9166052464d06
zerikv <zerikv@gmail.com>
2013-08-30 16:53:07 +0200

    Add delete node and reset input

--- mindmap.js
+++ mindmap.js
@@ -1,8 +1,9 @@
 function MindMap($scope) {
   $scope.tree = {'title': 'Root'};
+  $scope.input = {'title': ''};
 
-  $scope.add_child = function(parent_node, title) {
-    var child_node = {'title': title};
+  $scope.add_child = function(parent_node) {
+    var child_node = {'title': $scope.input.title, 'parent': parent_node};
 
     if (parent_node.children) {
       parent_node.children.push(child_node);
@@ -10,11 +11,18 @@ function MindMap($scope) {
     else {
       parent_node.children = [child_node];
     }
+
+    $scope.input.title = '';
   };
 
-  /*
-  $scope.delete_child = function() {
-    // @todo Find out how to do deletion.
+  $scope.delete_child = function(child_node_to_delete) {
+    var parent = child_node_to_delete.parent;
+    for(var i=0; i<parent.children.length; i++) {
+      var child_node = parent.children[i];
+      if (child_node == child_node_to_delete) {
+        parent.children.splice(i,1);
+        break;
+      }
+    } 
   };
-  */
 }
